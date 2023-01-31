afdf7d7d3b92cbd2dd1fbfee1e59a0baf95dbdfc
zhansulu1986 <123593579+zhansulu1986@users.noreply.github.com>
2023-01-30 21:07:51 -0500

    Update FirstGitHubClass.java
    hey changed to hello

--- src/Day1GitHub/FirstGitHubClass.java
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package Day1GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hey GitHub");
+        System.out.println("Hello GitHub");
         System.out.println(1+1);
     }
 }
